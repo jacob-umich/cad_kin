9b8b4880aa3a5e859ba30c00ea7da4b300587c25
jacobpavelka <86691427+jacobpavelka@users.noreply.github.com>
2025-05-02 21:10:04 -0400

    parametric nodes

--- src/cad_kin/linear_parametric_node.py
+++ src/cad_kin/linear_parametric_node.py
@@ -23,4 +23,22 @@ class LinearParametricNode(Node):
             self.constant_obj: b_map
         })
 
-    
+class ParametricNode(Node):
+    b_parametric = True
+    def __init__(self,node_i,parameter):
+        self.dof = node_i.dof
+        self.x_p = Parameter(parameter[0])
+        self.y_p = Parameter(parameter[1])
+        self.constant_obj = Parameter("1")
+        self.pos = ParametricConstraint({
+            self.x_p:np.array([[1,0]]),
+            self.y_p:np.array([[0,1]]),
+            self.constant_obj: node_i.pos
+        })
+    def get_map(self,n_dofs):
+        b_map = self.node_i.get_map(n_dofs)
+        diff_map= self.node_j.get_map(n_dofs)-self.node_i.get_map(n_dofs)
+        return ParametricConstraint({
+            self.param_obj:diff_map,
+            self.constant_obj: b_map
+        })
